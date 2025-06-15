aed62153690e7762e81152ac266eec96f7c54de7
abeetha <abeethaabeysundera@gmail.com>
2025-06-15 09:35:39 +0530

    Created user delete function

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,3 +67,11 @@ export function loginUser(req, res) {
             }
     })
 }   
+
+export function deleteUser(req, res) {
+    User.deleteOne({email: req.body.email }).then(() => {
+        res.json({
+            message: "User deleted"
+        })
+    })
+}
